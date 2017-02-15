e3c0ff8cb8edb6d26b074fb18cdb301ba53ccb37
dlskawns96 <dlskawns96@gmail.com>
2017-02-15 17:26:38 +0900

    dads

---
+++ Assets/Control.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Control : MonoBehaviour {
+
+    public int speed = 10;
+    public float jumpForce = 20f;
+    private bool grounded = true;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        float xMove = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        float yMove = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+
+        GetComponent<Rigidbody2D>().velocity = new Vector2(xMove * speed, GetComponent<Rigidbody2D>().velocity.y);
+	}
+
+    void Update()
+    {
+        if(grounded && Input.GetKeyDown(KeyCode.Space))
+        {
+            grounded = false;
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(0, jumpForce));
+        }
+    }
+}
