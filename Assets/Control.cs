0c433ce6a4035679a11d5a8af0b82a8f297162d4
dlskawns96 <dlskawns96@gmail.com>
2017-02-18 15:27:58 +0900

    abc

--- Assets/Control.cs
+++ Assets/Control.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Control : MonoBehaviour {
 
     public int speed = 10;
-    public float jumpForce = 20f;
+    public float jumpForce = 100f;
     private bool grounded = true;
 	// Use this for initialization
 	void Start () {
@@ -15,8 +15,6 @@ public class Control : MonoBehaviour {
 	// Update is called once per frame
 	void FixedUpdate () {
         float xMove = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
-        float yMove = Input.GetAxis("Vertical") * speed * Time.deltaTime;
-
         GetComponent<Rigidbody2D>().velocity = new Vector2(xMove * speed, GetComponent<Rigidbody2D>().velocity.y);
 	}
 
@@ -27,5 +25,13 @@ public class Control : MonoBehaviour {
             grounded = false;
             GetComponent<Rigidbody2D>().AddForce(new Vector2(0, jumpForce));
         }
+
+        //출동체크해서 땅에 닿으면 grounded를 true로 해주기
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+            grounded = true;
     }
 }
