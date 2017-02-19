09c4a7a7d117f628da2b18cd16860ea4ffa4f87c
dlskawns96 <dlskawns96@gmail.com>
2017-02-19 14:56:51 +0900

    x

--- Assets/Control.cs
+++ Assets/Control.cs
@@ -4,32 +4,38 @@ using UnityEngine;
 
 public class Control : MonoBehaviour {
 
-    public int speed = 10;
-    public float jumpForce = 100f;
+    public float speed = 5f;
+    
+    private Rigidbody2D rb2d;
+    private Vector2 curVel;
+    private float jumpPower = 150f;
     private bool grounded = true;
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void FixedUpdate () {
-        float xMove = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
-        GetComponent<Rigidbody2D>().velocity = new Vector2(xMove * speed, GetComponent<Rigidbody2D>().velocity.y);
-	}
+    
+    void Start()
+    {
+        rb2d = gameObject.GetComponent<Rigidbody2D>();
+        curVel = rb2d.velocity;
+    }
+
 
     void Update()
     {
-        if(grounded && Input.GetKeyDown(KeyCode.Space))
-        {
-            grounded = false;
-            GetComponent<Rigidbody2D>().AddForce(new Vector2(0, jumpForce));
-        }
-
-        //출동체크해서 땅에 닿으면 grounded를 true로 해주기
+        
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    void FixedUpdate()
+    {
+        curVel.x = Input.GetAxis("Horizontal") * speed;
+        rb2d.velocity = curVel;
+
+        if (grounded && Input.GetKeyUp(KeyCode.Space))
+        {
+            grounded = false;
+            rb2d.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
+        }
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Ground")
             grounded = true;
