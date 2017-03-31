3ad27851c83d1c7b7e17058c3d790306d544ec86
dlskawns96 <dlskawns96@gmail.com>
2017-04-01 01:24:01 +0900

    Lighting 효과 제작중

---
+++ Assets/Scripts/FadeDownLight.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeDownLight : MonoBehaviour {
+
+    private Light light;
+
+    private float start;
+    private float end;
+    private float t = 0.0f;
+    private bool isDown = true;
+
+    void Start()
+    {
+        light = GetComponent<Light>();
+        start = light.intensity;
+
+        end = light.intensity / 3;
+    }
+
+    void Update()
+    {
+
+        if (light.intensity == start)
+        {
+            isDown = true;
+            t = 0;
+        }
+        else if (light.intensity == end)
+        {
+            isDown = false;
+            t = 0;
+        }
+        t += Time.deltaTime;
+
+        if(isDown)
+            light.intensity = Mathf.Lerp(start, end, t / 2);
+        else
+            light.intensity = Mathf.Lerp(end, start, t / 2);
+
+    }
+}
