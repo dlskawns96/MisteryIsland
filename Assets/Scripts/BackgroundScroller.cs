389e9cc722b16b46842f8379b85f62d8f5b286eb
NamJunLee <dlskawns96@gmail.com>
2017-04-04 18:45:58 +0900

    looping sky

---
+++ Assets/Scripts/BackgroundScroller.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundScroller : MonoBehaviour {
+
+    public float speed;
+    public static BackgroundScroller current;
+    public Material mat;
+
+
+    float pos = 0;
+
+	// Use this for initialization
+	void Start () {
+        current = this;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        pos += speed;
+        if (pos > 1.0f)
+            pos -= 1.0f;
+
+        mat.mainTextureOffset = new Vector2(pos, 0);
+	}
+}
---
+++ Assets/Scripts/RepeatBackground.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RepeatBackground : MonoBehaviour {
+
+    private BoxCollider2D boxCollider;
+    private float groundHorizontalLength;
+
+	// Use this for initialization
+	void Start () {
+        boxCollider = GetComponent<BoxCollider2D>();
+        groundHorizontalLength = boxCollider.size.x;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (transform.position.x < -groundHorizontalLength)
+            repositioningBackgrounds();
+	}
+
+    private void repositioningBackgrounds()
+    {
+        Vector3 groundOffset = new Vector3(groundHorizontalLength * 2f, 0, 5.5f);
+        transform.position = new Vector3(transform.position.x + groundOffset.x, 0, 5.5f);
+          
+    }
+}
