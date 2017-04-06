263b5553318ac4e431eb21997b5e2a788134314a
dlskawns96 <dlskawns96@gmail.com>
2017-04-07 01:03:09 +0900

    카메라 이동 - raycast

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -3,9 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour {
-    
-    private void LateUpdate()
+
+    private float rayDistance;
+    private bool hitLeft, hitRight;
+    private int mask;
+
+    private void Start()
     {
+        mask = 1 << LayerMask.NameToLayer("EndPoint");
+        rayDistance = (GetComponent<BoxCollider2D>().size.x * 2) + 0.1f;
+    }
+
+    private void Update()
+    {
+        Ray2D leftRay = new Ray2D(transform.position, Vector2.left);
+        Ray2D rightRay = new Ray2D(transform.position, Vector2.right);
+
+        hitLeft = Physics2D.Raycast(leftRay.origin, leftRay.direction, rayDistance, mask);
+        hitRight = Physics2D.Raycast(rightRay.origin, rightRay.direction, rayDistance, mask);
+    }
+
+    private void LateUpdate()
+    {       
         move();
     }
     
@@ -13,11 +32,11 @@ public class CameraController : MonoBehaviour {
     {
         Vector3 moveVelocity = Vector3.zero;
 
-        if (Input.GetAxisRaw("Horizontal") < 0)
+        if (Input.GetAxisRaw("Horizontal") < 0 && !hitLeft)
         {
             moveVelocity = Vector3.left;
         }
-        else if (Input.GetAxisRaw("Horizontal") > 0)
+        else if (Input.GetAxisRaw("Horizontal") > 0 && !hitRight)
         {
             moveVelocity = Vector3.right;
         }
